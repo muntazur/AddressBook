60f6cbc0c2d09bb65135c4941ba894d754d3da51
muntazur <muntaz.servant.creator@gmail.com>
2018-12-08 10:48:43 +0600

    sign up option in front end as well as corresponding backend work properly

--- signup.php
+++ signup.php
@@ -2,31 +2,80 @@
 
 	require 'db_connect.php';
     
-    $name = "Md Muntazur Rahman";
-    $email = "muntaz.servant.creator@gmail.com";
-    $pasw = "123456";
+    if(isset($_POST['u_btn']))
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+
+        if($password != $confirm_password)echo "Password Mismatched!<br>";
+        else
+        {
+            $sql = "select * from users where email = '$email'";
+            $result = mysqli_query($connection,$sql);
+            $row = mysqli_fetch_array($result);
+
+            if($row['email'] == $email)echo "Already have an account.<br>";
+            else 
+            {
+                $sql = "insert into users (name,email,password) values ('$name', '$email','$password')";
+                $result = mysqli_query($connection,$sql);
+            }
 
 
-
-    $sql = "select email from users where email = '$email'";
-    $result = mysqli_query($connection,$sql);
-
-
-    if($result)
-    {   
-    	$existCounter=mysqli_num_rows($result);
-    	echo $existCounter."<br>";
-    	if($existCounter==0)
-    	{
-    		$sql = "insert into users (name,email,password) values ('$name','$email','$pasw')";
-    		mysqli_query($connection,$sql);
-    	}
-    	else
-    	{
-    		echo "User already exists<br>";
-    	}
+        }
     }
-    else 
-    	echo mysqli_error($connection);
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title>
+            LogIn
+        </title>
+
+        <link rel = "stylesheet" href = "style.css">
+    </head>
+
+    <body>
+
+        <form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method = "post">
+            <div class = "login-box">
+
+                <h4> Sign Up </h4>
+
+                <div class = "textbox">
+                
+                    <input type="text" name="name" placeholder="Full-name...." value="" required>
+                </div>
+
+                <div class = "textbox">
+                
+                    <input type="email" name="email" placeholder="E-mail...." value="" required>
+                </div>
+
+                <div class = "textbox">
+                
+                    <input type="password" name="password" placeholder="password...." value="" required>
+                </div>
+
+
+                <div class = "textbox">
+                
+                    <input type="password" name="confirm_password" placeholder="confirm-password...." value="" required>
+                </div>
+
+                <input class="btn" type="submit" name="u_btn" value = "register">
+
+            </div>
+
+        </form>
+
+
+    </body>
+</html>
